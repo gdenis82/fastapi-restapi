12f79ef56cf485f6049fe99de6c1e3935f8bfac4
Denis Gaev <denisgaev@live.ru>
2026-02-10 01:15:18 +0500

    Set up SQLAlchemy base and session factory for database interaction

---
+++ app/db/session.py
@@ -0,0 +1,7 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+from app.core.config import settings
+
+engine = create_async_engine(settings.database_url, echo=False)
+
+SessionLocal = async_sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False)
