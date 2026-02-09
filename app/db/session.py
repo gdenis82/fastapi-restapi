bc11fc3726245c7736ac9a88c9032f43f0597ab2
Denis Gaev <denisgaev@live.ru>
2026-02-10 01:49:23 +0500

    Refactor `session.py` to use `get_settings` for settings initialization

--- app/db/session.py
+++ app/db/session.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
-from app.core.config import settings
+from app.core.config import get_settings
+
+settings = get_settings()
 
 engine = create_async_engine(settings.database_url, echo=False)
 
