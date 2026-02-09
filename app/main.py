eead70871b7de4835c1b895df0deee1f58bcd180
Denis Gaev <denisgaev@live.ru>
2026-02-10 01:18:44 +0500

    Include `buildings` and `organizations` routers in FastAPI application

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import Depends, FastAPI
+from app.routers.buildings import router as buildings_router
 from app.routers.deps import verify_api_key
-
+from app.routers.organizations import router as organizations_router
 
 app = FastAPI(title="Organizations Directory API")
 
-
+app.include_router(buildings_router)
+app.include_router(organizations_router)
 
 
 @app.get(
