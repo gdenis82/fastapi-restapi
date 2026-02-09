fbd8d4bf9dadc4fb6239d036ceef42e87bc3acba
Denis Gaev <denisgaev@live.ru>
2026-02-10 01:15:46 +0500

    Add initial SQLAlchemy models for `Building`, `Organization`, `Phone`, and `Activity`

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.activity import Activity
+from app.models.building import Building
+from app.models.organization import Organization
+from app.models.phone import Phone
+
+__all__ = ["Activity", "Building", "Organization", "Phone"]
